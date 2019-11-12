ba05949e458b9605ac6dc6bf0b570356d787f89f
diego moreno <da.moreno17@uniandes.edu.co>
2019-11-12 18:36:25 +0000

    parte_1 adelantos

--- parte_1.cpp
+++ parte_1.cpp
@@ -3,6 +3,26 @@
 #include <fstream>
 using namespace std;
 
+void integral ();
+
+float func(float w, float y) 
+{ 
+    return (-w*y); 
+}
+
+void euler(float x, float y, float h, float x_0) 
+{ 
+    float t= 0; 
+  
+    while (x_0 < x) { 
+        t = y; 
+        y = y + h * func(x_0, y); 
+        x_0 = x_0 + h; 
+    } 
+  
+    cout << y << endl; 
+}
+
 int main(){
     
     float res_a ;
@@ -10,13 +30,18 @@ int main(){
     float delta_x = 0.1;
     float w = 0.1;
     float y_0 = 1;
+    float k = 4/w;
     
-    for (int i = 0, i < 4/w, i++){
+    for (int i = 0; i < k, i++;){
         res_a = (y_0) + (delta_x * w * y_0);
         res_b = (y_0) / (1 + delta_x * w);
     }
-    )
+    
     
     
     return 0;
+}
+
+void integral (){
+    
 }
