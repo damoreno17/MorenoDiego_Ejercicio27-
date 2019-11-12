8a5b749a39232032cb7f342dcc6d5a05a81efcdf
diego moreno <da.moreno17@uniandes.edu.co>
2019-11-12 18:09:38 +0000

    parte_1 adelanto

--- parte_1.cpp
+++ parte_1.cpp
@@ -5,6 +5,18 @@ using namespace std;
 
 int main(){
     
+    float res_a ;
+    float res_b ;
+    float delta_x = 0.1;
+    float w = 0.1;
+    float y_0 = 1;
+    
+    for (int i = 0, i < 4/w, i++){
+        res_a = (y_0) + (delta_x * w * y_0);
+        res_b = (y_0) / (1 + delta_x * w);
+    }
+    )
+    
     
     return 0;
 }
